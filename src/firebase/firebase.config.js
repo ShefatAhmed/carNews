90a2c96d9359fded0ef63def272e611816ab59be
unknown <shefata69@gmail.com>
2023-09-26 00:06:17 +0600

    last

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId
+  apiKey: "AIzaSyDLo6ID9882S6mLHptbgGVNQDm6GGIERM4",
+  authDomain: "bistro-boss-cf4b2.firebaseapp.com",
+  projectId: "bistro-boss-cf4b2",
+  storageBucket: "bistro-boss-cf4b2.appspot.com",
+  messagingSenderId: "477715789240",
+  appId: "1:477715789240:web:cff4170ec9548ef7b6cb4d"
 };
 
 // Initialize Firebase
